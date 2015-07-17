b44df1b3ff397331f5d79a9d72338d566a9c1b5b
Maulin Pathare <mpathare@gmail.com>
2015-07-17 12:39:08 -0400

    Updated steps
    use Firefox 35
    add test-server gulp task
    
    LGTMs
    closes #22 closes #15 close #16 closes #20 closes #21

--- generators/app/templates/_install-firefox-30.sh
+++ generators/app/templates/_install-firefox-30.sh
@@ -1,10 +1,10 @@
 set -x
 set -e
 if [ ! -e firefox ]; then
-  wget https://s3.amazonaws.com/crowdtap-ci/firefox-30.0.tar.bz2
+  wget https://s3.amazonaws.com/crowdtap-ci/firefox-35.0.1.tar.bz2
   tar -xjf firefox-30.0.tar.bz2
 fi
 sudo rm -rf  /opt/firefox
 sudo rm -rf  /usr/bin/firefox
-sudo cp -r firefox /opt/firefox30
-sudo ln -s /opt/firefox30/firefox /usr/bin/firefox;
+sudo cp -r firefox /opt/firefox35
+sudo ln -s /opt/firefox35/firefox /usr/bin/firefox;
--- generators/app/templates/_steps.js
+++ generators/app/templates/_steps.js
@@ -3,23 +3,38 @@
 (function() {
   'use strict';
 
-  var express, should, _;
-
-  _       = require('underscore');
-  should  = require('should');
-  express = require('express');
+  var _       = require('underscore');
+  var should  = require('should');
+  var express = require('express');
+  var chalk   = require('chalk');
+  var prompt  = require('prompt');
   require('../../spec/factories/index');
+
+  function clearRoutes(_this) {
+    _this.app._router.stack = _.reject(_this.app._router.stack, { name: '<anonymous>' });
+  }
 
   module.exports = function() {
     this.World = require('../support/world').World;
+    var queue = [];
+    var visitParams = {};
 
     this.Before(function(callback) {
+      queue = [];
+      visitParams = {};
       this.app = require('../support/world').App;
       this.port = require('../support/world').Port;
+
+      this.app.use(function(req, res, next) {
+        queue.push({ verb: req.method, url:  req.url, body: req.body });
+        return next();
+      });
+
       this.browser = Browser;
       this.browser.manage().timeouts().setScriptTimeout(100.0 * 1000);
       this.browser.manage().timeouts().pageLoadTimeout(100.0 * 1000);
       this.browser.manage().timeouts().implicitlyWait(3.0 * 1000);
+      this.browser.manage().window().setSize(1024, 768);
       this.visitParams = {};
       return callback();
     });
@@ -30,8 +45,28 @@
           return callback();
         };
       })());
-      this.app.routes.get = [];
-      this.app.routes.post = [];
+      clearRoutes(this);
+    });
+
+    this.Given(/^the "([^"]+)" query parameter is set to "([^"]+)"$/, function(name, value, callback) {
+      this.visitParams[name] = value;
+      callback();
+    });
+
+    this.Given(/^the user is on a mobile device$/, function(callback) {
+      this.browser.manage().window().setSize(320, 480);
+      callback();
+    });
+
+    this.Given(/^the stubs are cleared$/, function(callback) {
+      clearRoutes(this);
+      callback();
+    });
+
+    this.Given(/^the "([^"]*)" window parameter is set to "([^"]*)"$/, function(paramName, paramValue, callback) {
+      this.browser.executeScript(function(param, value) {
+        window[param] = value;
+      }, paramName, paramValue).then(callback);
     });
 
     this.Then(/^I should (not )?see "([^"]*)" ([^:]+)$/, function(negation, text, element, callback) {
@@ -46,11 +81,12 @@
       }
 
       text = text.replace(/\&quot;/g, '"');
-      return this.browser.findElement(path).then((function(element) {
-        return element.isDisplayed().then(function(isVisible) {
+      this.browser.findElement(path).then((function(element) {
+        element.isDisplayed().then(function(isVisible) {
           if (isVisible) {
-            return element.getText().then(function(html) {
-              html = html.replace(/[\n\r]/g, ' ').trim().replace(/\s{2,}/g, ' ');
+            element.getText().then(function(html) {
+              html = html.replace(/[\n\r]/g, ' ').trim().replace(/\s{2,}/g, ' ').toLowerCase();
+              text = text.toLowerCase();
               if (typeof negation !== 'undefined') {
                 html.should.not.include(text, "found '" + text + "' within '" + selector);
               } else {
@@ -74,7 +110,10 @@
       return this.browser.isElementPresent({
         css: "" + selector2 + " " + selector1
       }).then(function(found) {
-        found.should.eql(!negation);
+        if (!negation) {
+          negation = "";
+        }
+        found.should.eql(!negation, "expected to " + negation + " see " + selector2 + " " + selector1);
         return callback();
       });
     });
@@ -143,19 +182,52 @@
       })(this));
     });
 
+    this.Then(/^(.*) should be at the top of the page$/, function(element, callback) {
+      var selector = this.selectorFor(element);
+
+      this.browser.executeScript(function(selector) {
+        var element = document.querySelector(selector);
+        var rect    = element.getBoundingClientRect();
+
+        return rect.top;
+      }, selector).then(function(top) {
+        Math.round(top).should.eql(45);
+        callback();
+      });
+    });
+
     this.When(/^I select "([^"]*)" from (.*)$/, function(selectText, path, callback) {
       var selector;
       selector = this.selectorFor(path);
+
+      var clickLink = function clickLink() {
+        this.browser.findElement({
+          linkText: selectText
+        }).click().then(callback);
+      }.bind(this);
+
       this.browser.findElement({
         css: selector
-      }).click();
-      this.browser.findElement({
-        linkText: selectText
-      }).click();
-      return callback();
+      }).click().then(clickLink);
     });
 
-    this.When(/^I click (.*)$/, function(path, callback) {
+    this.When(/^I fill in (.+) with "([^"]*)"$/, function(namedElement, text, callback) {
+      var selector = {css: this.selectorFor(namedElement)};
+      var element = this.browser.findElement(selector);
+      element.clear().then(function() {
+        element.sendKeys(text).then(callback);
+      });
+    });
+
+    this.When(/^I click "([^"]*)"$/, function(text, callback) {
+      var element = this.browser.findElement({
+        xpath: "//*[contains(text(), '" + text + "')]"
+      });
+
+      element.click().then(callback);
+    });
+
+    this.When(/^I click ([^"]*)$/, function(path, callback) {
       var selector = this.selectorFor(path);
       var options  = {};
 
@@ -205,7 +277,155 @@
       });
     });
 
+    this.Then(/^the "([^"]+)" attribute on (.+) should be "([^"]+)"$/, function(attribute, element, value, callback) {
+      var cssSelector = this.selectorFor(element);
+      value = value.replace(/`/g, '"');
+      this.browser.findElement({
+        css: cssSelector
+      }).getAttribute(attribute).then(function(actual) {
+        actual.should.eql(value, "expected " + attribute + " to be " + value);
+        return callback();
+      });
+    });
+
+    this.Then(/^the url of the window should contain "([^"]*)"$/, function(url, callback) {
+      var expectedUrl = url;
+
+      this.browser.getCurrentUrl().then((function(actualUrl) {
+        should.exist(actualUrl.match(expectedUrl));
+        callback();
+      }));
+    });
+
     // Write your API steps below this
 
+    this.Given(/^(?:a (.*) request to )?the "([^"]+)" API endpoint returns the following(?: (.*)):$/, function(httpAction, path, factory, response, callback) {
+      var object;
+      if (factory === "JSON") {
+        object = JSON.parse(response);
+      } else {
+        object = this.Factory(factory, JSON.parse(response));
+      }
+      apiResponse.bind(this)(path, httpAction, object);
+
+      callback();
+    });
+
+    this.Given(/^(?:a (.*) request to )?the "([^"]+)" API endpoint returns with a "(\d+)" status$/, function(httpAction, path, httpStatus, callback) {
+      apiResponse.bind(this)(path, httpAction, httpStatus);
+      callback();
+    });
+
+    this.Then(/^I should (not )?have made a (.*) request to "([^"]*)" with the following (.*) params:$/, function(negation, verb, url, factory, params, callback) {
+      if (typeof(params) === 'function') {
+        callback = params;
+        params = undefined;
+      } else {
+        params = this.Factory(factory, JSON.parse(params));
+      }
+
+      checkRequests(negation, verb, url, params, callback);
+    });
+
+    this.Then(/^I should (not )?have made a (.*) request to "([^"]*)"(?: with params:)?$/, function(negation, verb, url, params, callback) {
+      if (typeof(params) === 'function') {
+        callback = params;
+        params = undefined;
+      } else {
+        params = JSON.parse(params);
+      }
+
+      checkRequests(negation, verb, url, params, callback);
+    });
+
+    var checkRequests = function(negation, verb, url, params, callback) {
+      var match  = false;
+      var actual = {verb: null, params: null};
+      var retries = 10;
+
+      var interval = setInterval(function() {
+        retries -= 1;
+        if (match || retries < 0) {
+          if (negation) {
+            match.should.eql(false, "unexpected " + verb + " request made to " + url + " with " + params);
+          } else {
+            match.should.eql(true, verb + " request not made to " + url + " with " + JSON.stringify(params, null, 2) + ", actual request was " + actual.verb + " with " + JSON.stringify(actual.params, null, 2));
+          }
+          clearInterval(interval);
+          return callback();
+        } else {
+          queue.forEach(function(element) {
+            if (element.url === url) {
+              actual.verb = element.verb;
+              actual.params = element.body;
+            }
+            if (actual.verb === verb && element.url === url) {
+              if (!params || compareObjects(params, actual.params)) {
+                match = true;
+              }
+            }
+          }.bind(this), 100);
+        }
+      }.bind(this));
+    };
+
+    var compareObjects = function(expected, actual) {
+      var match = true;
+      var BreakException = {};
+
+      if (Object.keys(expected).length !== Object.keys(actual).length) {
+        // Uncomment this to debug
+        // console.log();
+        // console.log("Expected: " + Object.keys(expected).sort().join(", "));
+        // console.log("Actual: " + Object.keys(actual).sort().join(", "));
+        // console.log();
+        // console.log("Expected: " + JSON.stringify(expected));
+        // console.log("Actual: " + JSON.stringify(actual));
+        return false;
+      }
+
+      try {
+        Object.keys(expected).forEach(function(key) {
+          if (!match || typeof expected[key] !== typeof actual[key]) {
+            // console.log(typeof expected[key]);
+            // console.log(typeof actual[key]);
+            match = false;
+            throw BreakException;
+          }
+
+          if (expected[key] === null) {
+            match = actual[key] === null;
+          } else if (typeof expected[key] === 'object') {
+            match = compareObjects(expected[key], actual[key]);
+          } else if (expected[key] !== actual[key]) {
+            // Uncomment this to debug
+            // console.log();
+            // console.log("Invalid parameter:");
+            // console.log(key);
+            // console.log("Expected: " + expected[key]);
+            // console.log("Actual: " + actual[key]);
+
+            match = false;
+          }
+        });
+      } catch(e) {
+        if (e !== BreakException) {
+          throw e;
+        }
+      }
+
+      return match;
+    };
+
+    var apiResponse = function apiResponse(path, httpAction, response) {
+      var action = httpAction || "GET";
+      this.app[action.toLowerCase()](path, function(req, res) {
+        if (typeof(response) === 'object') {
+          res.json(response);
+        } else {
+          res.send(response);
+        }
+      });
+    };
   };
 }).call(this);
