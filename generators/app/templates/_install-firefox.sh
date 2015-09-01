249fa7b582fc461e979442a97cf483e680de5aa4
Maulin Pathare <mpathare@gmail.com>
2015-09-01 17:27:52 -0400

    Fix firefox install script and rename it

--- generators/app/templates/_install-firefox.sh
+++ generators/app/templates/_install-firefox.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 if [ ! -e firefox ]; then
   wget https://s3.amazonaws.com/crowdtap-ci/firefox-35.0.1.tar.bz2
-  tar -xjf firefox-30.0.tar.bz2
+  tar -xjf firefox-35.0.1.tar.bz2
 fi
 sudo rm -rf  /opt/firefox
 sudo rm -rf  /usr/bin/firefox
