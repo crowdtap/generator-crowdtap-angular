d0416138e148c4f6107c7405cf81cdb4b3457482
Maulin Pathare <mpathare@gmail.com>
2015-09-02 15:10:18 -0400

    Add prompt for sentry and capture exceptions with raven
    LGTMs
      Jico
      Rob
    
    Closes #26

--- generators/app/templates/_app.js
+++ generators/app/templates/_app.js
@@ -1,1 +1,38 @@
 angular.module('<%= fullAppName %>', ['config', 'ngResource']);
+
+// @ngInject
+function Config($provide) {
+  "use strict";
+
+  // Log Angular exceptions to Sentry
+  $provide.decorator('$exceptionHandler', [
+    '$delegate', function($delegate) {
+      return function(exception, cause) {
+        $delegate(exception, cause);
+        return Raven.captureException(exception, {
+          extra: { cause: cause }
+        });
+      };
+    }
+  ]);
+
+  // Log HTTP response errors to Sentry
+  $provide.factory('ravenHttpInterceptor', [
+    '$q', function($q) {
+      return {
+        responseError: function(rejection) {
+          Raven.captureException(new Error('HTTP response error'), {
+            extra: {
+              config: rejection.config,
+              status: rejection.status,
+              data: rejection.data,
+            }
+          });
+          return $q.reject(rejection);
+        }
+      };
+    }
+  ]);
+}
+
+angular.module('<%= fullAppName %>').config(Config);
